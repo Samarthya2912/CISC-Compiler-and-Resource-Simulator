5d006af62392fc7062e323ce3189a2b8381d898c
Samarthya <samarthya55@gmail.com>
2022-04-12 21:50:48 +0530

    memory UI

---
+++ src/components/Memory.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Memory.css"
+
+const rows = 16, cols = 16;
+const memory = [];
+for (let i = 0; i < rows; i++) {
+    memory.push([]);
+    for(let j = 0; j < cols; j++) {
+        memory[i].push("00000000");
+    }
+}
+
+const Memory = () => {
+  return (
+    <div className="memory">
+      {memory.map((row, row_index) => (
+        <div className="row" key={row_index}>
+          {row.map((word, col_index) => (
+            <div key={col_index} className="word">
+              {word}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Memory;
