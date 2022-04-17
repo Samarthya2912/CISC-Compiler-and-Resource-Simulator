40512261f9dab891db29002d43c3f36415de892d
Samarthya <samarthya55@gmail.com>
2022-04-17 19:42:54 +0530

    memoryReferenceInstructions except BSA

--- src/functions/memoryReferenceInstructions.js
+++ src/functions/memoryReferenceInstructions.js
@@ -1,17 +1,22 @@
 import bitset from "../bitset";
 
 let memoryReferenceInstructionFunction = (instruction,currentState) => {
+  let wordIndex = (addressIndex) => [Math.floor(addressIndex/8),addressIndex%8];
   let opcode = instruction.to_string().substr(0,4);
+  let opcodebits = instruction.subbitset(0,4);
   let address = instruction.subbitset(4,12);
   console.log(address.to_string());
 
   currentState.registers["IR"].copy(instruction);
   currentState.registers["AR"].copy(address);
 
-  console.log("yaha ddekho",address,currentState.registers["AR"]);
-  
-
-  let wordIndex = (addressIndex) => [Math.floor(addressIndex/8),addressIndex%8];
+  if(opcodebits.to_decimal() > 6) {
+    let addressIndex = currentState.registers["AR"].to_decimal();
+    let [i,j] = wordIndex(addressIndex);
+    currentState.registers["AR"].copy(currentState.MEMORY[i][j].subbitset(4,12));
+    opcode = "0"+opcode.substr(1,3);
+    console.log({newopcode: opcode});
+  }
 
   if(opcode === "0000" || opcode === "0001") {
     let addressIndex = currentState.registers["AR"].to_decimal();
@@ -27,15 +32,31 @@ let memoryReferenceInstructionFunction = (instruction,currentState) => {
     currentState.registers["AC"].copy(currentState.registers["DR"]);
   } 
   else if(opcode === "0011") {
-    console.log("here");
     let addressIndex = currentState.registers["AR"].to_decimal();
-    console.log({ar:currentState.registers["AR"].to_string(),addressIndex});
     let [i,j] = wordIndex(addressIndex);
     currentState.MEMORY[i][j].copy(currentState.registers["AC"]);
-    console.log({i,j,mem:currentState.MEMORY[i][j]});
   }
+  else if(opcode === "0100") {
+    let addressIndex = currentState.registers["AR"].to_decimal();
+    let [i,j] = wordIndex(addressIndex);
+    currentState.registers["PC"].copy(currentState.registers["AC"]);
+  }
+  else if(opcode === "0101") {
+    /* for phase 2 */
+  }
+  else if(opcode === "0110") {
+    let addressIndex = currentState.registers["AR"].to_decimal();
+    let [i,j] = wordIndex(addressIndex);
+    currentState.registers["DR"].copy(currentState.MEMORY[i][j]);
+    currentState.registers["AC"].copy(currentState.MEMORY[i][j]);
+    currentState.registers["AC"].add(bitset.hex2bin("0001"));
+    if(currentState.registers["AC"].isZero()) {
+      currentState.registers["PC"].add(bitset.hex2bin("001"));
+    }
+  }
+  else alert("Error in memoryReferenceInstructionFunction.");
 
-  currentState.registers["PC"].add(bitset.hex2bin("001"));
+  if(opcode !== "0100" && opcode !== "0101") currentState.registers["PC"].add(bitset.hex2bin("001"));
   return currentState;
 }
 
